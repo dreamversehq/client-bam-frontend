49b44826e0de5b51f00fea5575bd13395b087fc0
Abubakar Usman <usmansadik96@gmail.com>
2025-12-26 12:38:56 +0100

    fix: add .env.local to enable api url configurability

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AuthResponse, LoginRequest, RegisterRequest, Transaction, TransactionResponse, User } from '../types';
 
-const API_URL = 'https://client-bam-backend.onrender.com/v1';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://client-bam-backend.onrender.com/v1';
 
 const api = axios.create({
   baseURL: API_URL,
